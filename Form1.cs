ce6712bb3f92b619c7bd4439518bd961447e6b63
vannlymonm-bit <vannly.monm@sbacnon.ac.th>
2026-02-03 09:50:49 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
